88287129270777d9b66b25be81add639db635e9c
li125434104 <125434104@qq.com>
2015-09-06 18:20:18 +0800

    UIView动画练习

--- AnimationTest/ViewController.h
+++ AnimationTest/ViewController.h
@@ -6,6 +6,10 @@
 //  Copyright (c) 2015年 GOME. All rights reserved.
 //
 
+/**
+ *  CGAffineTransform 的 UIView 的动画练习
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
--- AnimationTest/ViewController.m
+++ AnimationTest/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
+typedef enum _animationType {
+    ViewMove,     //首尾移动
+    Scale,        //放大缩小
+    Rotation,     //旋转
+    Translation   //移动
+} animationType;
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *animationView;
 
 @end
 
@@ -16,7 +25,72 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"动画执行前的位置：%@",NSStringFromCGPoint(self.animationView.center));
+    
+    int i = 1;
+    
+    switch (i) {
+        case 0:
+            [self move];
+            break;
+        case 1:
+            [self scale];
+            break;
+        case 2:
+            [self rotation];
+            break;
+        case 3:
+            [self viewMove];
+            break;
+        default:
+            break;
+    }
+}
+
+//移动
+- (void)move {
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:2.f];
+    [UIView setAnimationDelegate:self];
+    self.animationView.center = CGPointMake(200, 300);
+    [UIView setAnimationDidStopSelector:@selector(didStopAnimation)];
+    [UIView commitAnimations];
+}
+
+//缩放
+- (void)scale {
+    [UIView animateWithDuration:2.f animations:^{
+        self.animationView.center = CGPointMake([UIScreen mainScreen].bounds.size.width / 2, 50);
+        self.animationView.transform = CGAffineTransformMakeScale(0.5, 0.5);
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+//旋转
+- (void)rotation {
+    [UIView animateWithDuration:2.f animations:^{
+        self.animationView.transform = CGAffineTransformMakeRotation(M_PI_2);
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+//移动
+- (void)viewMove {
+    [UIView animateWithDuration:2.f animations:^{
+        self.animationView.transform = CGAffineTransformMakeTranslation(100, 100);
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+- (void)didStopAnimation {
+    NSLog(@"动画执行完毕");
+    NSLog(@"动画执行完毕的位置：%@",NSStringFromCGPoint(self.animationView.center));
 }
 
 - (void)didReceiveMemoryWarning {
